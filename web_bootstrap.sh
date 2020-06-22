f8790ae667cc34f797ea175bb7172d7ec4977fcf
Michael Braun <michaelbraunbass@gmail.com>
2020-06-22 14:34:02 -0600

    Update

--- web_bootstrap.sh
+++ web_bootstrap.sh
@@ -15,31 +15,16 @@ until sudo git clone https://github.com/CheckPointSW/smart-console-extensions.gi
     sleep 1
 done
 
-# Start Docker
-sudo service nginx stop
-sudo docker run --rm -d -p 3000:3000 bkimminich/juice-shop
-
 # Gather IP address
 
 ip_address=$(dig +short myip.opendns.com @resolver1.opendns.com)
 
 # Modify Nginx Config to allow access to Juice Store
-sudo touch /etc/nginx/conf.d/juice.conf
-
-sudo chmod a+w /etc/nginx/conf.d/juice.conf
-
-sudo cat <<EOT >> /etc/nginx/conf.d/juice.conf
-server {
-  listen 80;
-  listen [::]:80;
-
-  server_name $ip_address;
-
-  location / {
-      proxy_pass http://localhost:3000/;
-  }
-}
-EOT
+rm /etc/nginx/sites-enabled/default/index.html
+cp /smart-console-extensions/examples/helloworld/css /etc/nginx/sites-enabled/default/
+cp /smart-console-extensions/examples/helloworld/js /etc/nginx/sites-enabled/default/
+cp /smart-console-extensions/examples/helloworld/extension.json /etc/nginx/sites-enabled/default/extension.json
+cp /smart-console-extensions/examples/helloworld/index.html /etc/nginx/sites-enabled/default/index.html
 
 sudo service nginx restart
 sleep 2
--- web_server.tf
+++ web_server.tf
@@ -2,7 +2,6 @@
 # Setup the userdata that will be used for the instance
 data "template_file" "userdata_setup" {
   template = "${file("userdata_setup.template")}"
-
   vars  = {
     name       = "${var.username}"
     logic = "${file("web_bootstrap.sh")}"
