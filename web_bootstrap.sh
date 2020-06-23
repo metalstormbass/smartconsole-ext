f55d4355b66713fc64feb71e3fe472d86df594c1
Michael Braun <michaelbraunbass@gmail.com>
2020-06-23 08:29:33 -0600

    Update web_bootstrap.sh

--- web_bootstrap.sh
+++ web_bootstrap.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo $name >> /home/mike/test.txt
-echo $git_addr >> /home/mike/test2.txt
-
 # Update and download Nginx
 until sudo apt-get update && sudo apt-get -y install nginx;do
     sleep 1
@@ -33,35 +30,23 @@ sudo mkdir /etc/nginx/ssl
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/C=CA/ST=AB/L=Calgary/O=Dis/CN=www.example.com"
 
 #Build Config File
-sudo touch /etc/nginx/sites-enabled/default
-sudo cat <<EOT >> /etc/nginx/sites-enabled/default
+sudo touch /etc/nginx/sites-enabled/smartconsoleEXT
+sudo cat <<EOT >> /etc/nginx/sites-enabled/smartconsoleEXT
 server {
-       
-        server_name _;
-        listen       80  default_server;
-   
-
-        listen 443 ssl default_server;
-        listen [::]:443 ssl default_server;
-        server_name _;
-        ssl_certificate /etc/nginx/ssl/nginx.crt;
-        ssl_certificate_key /etc/nginx/ssl/nginx.key;
-
-        root /var/www/html;
-
-        # Add index.php to the list if you are using PHP
-        index index.html index.htm index.nginx-debian.html;
-
-        server_name _;
-
-        location / {
-                # First attempt to serve request as file, then
-                # as directory, then fall back to displaying a 404.
-                try_files $uri $uri/ =404;
-        }
-
-        
+    server_name _;
+    listen       80  default_server;
+    return       404;
 }
+
+
+server {
+    listen 443 ssl;
+    server_name _;
+    ssl_certificate /etc/nginx/ssl/nginx.crt;
+    ssl_certificate_key /etc/nginx/ssl/nginx.key;
+    root /var/www/html;
+}
+
 
 
 EOT
